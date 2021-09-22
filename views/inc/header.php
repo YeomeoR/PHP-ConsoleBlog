a42bfcdbcb8bc30da5545235149864ceb640610f
Rob Yeomans <76974577+YeomeoR@users.noreply.github.com>
2021-09-22 15:10:22 +0100

    production commit

---
+++ views/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../../public/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    
+    <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
+    <div class="container">
